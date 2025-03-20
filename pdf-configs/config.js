7a6e4ad0c1204caa754c560ad5f5d9c018ac68e5
Naoki Maekawa <naokimaekawa@Naokis-MacBook-Pro.local>
2025-03-20 12:06:31 +0900

    Revis config.js

--- pdf-configs/config.js
+++ pdf-configs/config.js
@@ -12,5 +12,8 @@ module.exports = {
     "headerTemplate": "<style>\n  section {\n    margin: 0 auto;\n    font-size: 9px;\n  }\n</style>",
     "footerTemplate": "<section>\n  <div>\n    <span class=\"pageNumber\"></span>\n    / <span class=\"totalPages\"></span>\n  </div>\n</section>"
   },
+  launch_options: {
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  },
   stylesheet_encoding: "utf-8",
 };
